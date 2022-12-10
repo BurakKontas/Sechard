c023efe7838fd9d6c3c8786edb7807354eed9837
BurakKontas <aburakkontas@trakya.edu.tr>
2022-12-10 15:16:53 +0300

    Stack navigation added

--- App.js
+++ App.js
@@ -2,12 +2,37 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+//screens
+import { HomeScreen } from './src/screens/Contacts';
+
+
+
+const Stack = createNativeStackNavigator();
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerBackgroundColor:"#FFF",
+        headerShadowVisible:false,
+      }}>
+        <Stack.Screen name="Contacts" component={HomeScreen} 
+          options={{
+            headerRight:(props) => {
+              return <Text>Hello</Text> //<ContactsHeaderRight />
+            },
+            headerTitleAlign:'center',
+            headerTitleStyle:{
+              fontSize:25
+            }
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
